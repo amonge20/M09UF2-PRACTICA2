4905c0dc7e79223bab268ada5b91d85affe56609
lcastienc <lcastill@jaumebalmes.net>
2023-02-28 01:19:35 +0100

    html structure finished

---
+++ Luis/Index.js
@@ -0,0 +1,23 @@
+const songs = [
+  {
+    name: "Blood",
+    artist: "All My Friends Hate Me",
+    cover: "./Images/Blood_-_All_My_Friends_Hate_Me.jpeg",
+    audio: "./Songs/Blood_-_All_My_Friends_Hate_Me.mp3",
+    time: "03:31",
+  },
+  {
+    name: "Boys, Girls, Toys & Words",
+    artist: "Modern Pitch",
+    cover: "./Images/Boys,_Girls,_Toys_&_Words_-_Modern_Pitch.jpeg",
+    audio: "./Songs/Boys,_Girls,_Toys_&_Words_-_Modern_Pitch.mp3",
+    time: "05:57",
+  },
+  {
+    name: "Cold Inside",
+    artist: "RUDE",
+    cover: "./Images/Cold_Inside_-_Rude.jpeg",
+    audio: "./Songs/Cold_Inside_-_Rude.mp3",
+    time: "03:17",
+  },
+];
